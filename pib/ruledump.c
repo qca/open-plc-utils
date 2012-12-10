70eccfedf2b870b810287d831ce77fc3b6afbcc6
Charles Maier <cmaier@qca.qualcomm.com>
2012-12-09 22:06:24 -0500

    Add new modules ruledump, LinkStatus and getargv

---
+++ pib/ruledump.c
@@ -0,0 +1,219 @@
+/*====================================================================*
+ *   
+ *   Copyright (c) 2012 by Qualcomm Atheros.
+ *   
+ *   Permission to use, copy, modify, and/or distribute this software 
+ *   for any purpose with or without fee is hereby granted, provided 
+ *   that the above copyright notice and this permission notice appear 
+ *   in all copies.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL 
+ *   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL  
+ *   THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR 
+ *   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+ *   LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+ *   NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+ *   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *   
+ *--------------------------------------------------------------------*/
+
+/*====================================================================*
+ *   
+ *   void ruledump (struct rule * rule, FILE * fp);
+ *
+ *.  Qualcomm Atheros HomePlug AV Powerline Toolkit.
+ *:  Published 2012 by Qualcomm Atheros. ALL RIGHTS RESERVED.
+ *;  For demonstration and evaluation only. Not for production use.
+ *
+ *--------------------------------------------------------------------*/
+
+#ifndef RULEDUMP_SOURCE
+#define RULEDUMP_SOURCE
+
+#include <stdio.h>
+
+#include "../tools/memory.h"    
+#include "../plc/rules.h"    
+
+/*====================================================================*
+ *
+ *   cspec_dump (struct cspec * cspec);
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+void cspec_dump (struct cspec * cspec) 
+
+{
+	printf ("cspec->CSPEC_VERSION=%d\n", LE16TOH (cspec->CSPEC_VERSION));
+	printf ("cspec->VLAN_TAG=%d\n", LE16TOH (cspec->VLAN_TAG));
+	return;
+}
+
+
+/*====================================================================*
+ *
+ *   MMEClassifierDump (struct MMEClassifier * classifier);
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+void MMEClassifierDump (struct MMEClassifier * classifier) 
+
+{
+	char string [48];
+	printf ("classifier->CR_PID=0x%02X\n", (unsigned)(classifier->CR_PID));
+	printf ("classifier->CR_OPERAND=0x%02X\n", classifier->CR_OPERAND);
+	hexdecode (classifier->CR_VALUE, sizeof (classifier->CR_VALUE), string, sizeof (string));
+	printf ("classifier->CR_VALUE=%s\n", string);
+	return;
+}
+
+
+/*====================================================================*
+ *
+ *   PIBClassifierDump (struct PIBClassifier * classifier);
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+void PIBClassifierDump (struct PIBClassifier * classifier) 
+
+{
+	char string [48];
+	printf ("classifier->CR_PID=0x%08X\n", LE32TOH (classifier->CR_PID));
+	printf ("classifier->CR_OPERAND=0x%08X\n", LE32TOH (classifier->CR_OPERAND));
+	hexdecode (classifier->CR_VALUE, sizeof (classifier->CR_VALUE), string, sizeof (string));
+	printf ("classifier->CR_VALUE=%s\n", string);
+	return;
+}
+
+
+/*====================================================================*
+ *
+ *   MMERuleDump (struct MMERule * rule);
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+void MMERuleDump (struct MMERule * rule) 
+
+{
+	unsigned count;
+	printf ("rule->MCONTROL=%d\n", rule->MCONTROL);
+	printf ("rule->MVOLATILITY=%d\n", rule->MVOLATILITY);
+	printf ("rule->MACTION=%d\n", rule->MACTION);
+	printf ("rule->MOPERAND=%d\n", rule->MOPERAND);
+	printf ("rule->NUM_CLASSIFIERS=%d\n", rule->NUM_CLASSIFIERS);
+	for (count = 0; count < rule->NUM_CLASSIFIERS; count++) 
+	{
+		MMEClassifierDump (&rule->CLASSIFIER [count]);
+	}
+	cspec_dump (&rule->cspec);
+	printf ("\n");
+	return;
+}
+
+
+/*====================================================================*
+ *
+ *   PIBRuleDump (struct PIBRule * rule);
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+void PIBRuleDump (struct PIBRule * rule) 
+
+{
+	unsigned count;
+	printf ("rule->MCONTROL=%d\n", rule->MCONTROL);
+	printf ("rule->MVOLATILITY=%d\n", rule->MVOLATILITY);
+	printf ("rule->MACTION=%d\n", rule->MACTION);
+	printf ("rule->MOPERAND=%d\n", rule->MOPERAND);
+	printf ("rule->NUM_CLASSIFIERS=%d\n", LE32TOH (rule->NUM_CLASSIFIERS));
+	for (count = 0; count < LE32TOH (rule->NUM_CLASSIFIERS); count++) 
+	{
+		PIBClassifierDump (&rule->CLASSIFIER [count]);
+	}
+	cspec_dump (&rule->cspec);
+	printf ("\n");
+	return;
+}
+
+
+/*====================================================================*
+ *
+ *   classifier_priority_map_dump (struct classifier_priority_map * classifier_priority_map);
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+void classifier_priority_map_dump (struct classifier_priority_map * classifier_priority_map) 
+
+{
+	printf ("classifier_priority_map->Priority=%d\n", LE32TOH (classifier_priority_map->Priority));
+	PIBClassifierDump (&classifier_priority_map->CLASSIFIER);
+	return;
+}
+
+
+/*====================================================================*
+ *
+ *   auto_connection_dump (struct auto_connection * auto_connection);
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+void auto_connection_dump (struct auto_connection * auto_connection) 
+
+{
+	unsigned count;
+	printf ("auto_connection->MACTION=%d\n", auto_connection->MACTION);
+	printf ("auto_connection->MOPERAND=%d\n", auto_connection->MOPERAND);
+	printf ("auto_connection->NUM_CLASSIFIERS=%d\n", LE16TOH (auto_connection->NUM_CLASSIFIERS));
+	for (count = 0; count < LE16TOH (auto_connection->NUM_CLASSIFIERS); count++) 
+	{
+		PIBClassifierDump (&auto_connection->CLASSIFIER [count]);
+	}
+	cspec_dump (&auto_connection->cspec);
+	return;
+}
+
+
+/*====================================================================*
+ *
+ *   PIBClassifiersDump (struct PIBClassifiers * classifiers);
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+void PIBClassifiersDump (struct PIBClassifiers * classifiers) 
+
+{
+	unsigned count;
+	printf ("classifiers->priority_count=%d\n", LE32TOH (classifiers->priority_count));
+	printf ("classifiers->autoconn_count=%d\n", LE32TOH (classifiers->autoconn_count));
+	printf ("\n");
+	printf ("-------- PRIORITY MAPS --------\n\n");
+	for (count = 0; count < LE32TOH (classifiers->priority_count); count++) 
+	{
+		classifier_priority_map_dump (&classifiers->classifier_priority_map [count]);
+	}
+	printf ("\n");
+	printf ("-------- AUTO CONNECTIONS --------\n\n");
+	for (count = 0; count < LE32TOH (classifiers->autoconn_count); count++) 
+	{
+		auto_connection_dump (&classifiers->auto_connection [count]);
+	}
+	printf ("\n");
+	return;
+}
+
+
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+#endif
+
---
+++ plc/LinkStatus.c
@@ -0,0 +1,103 @@
+/*====================================================================*
+ *   
+ *   Copyright (c) 2011 by Qualcomm Atheros.
+ *   
+ *   Permission to use, copy, modify, and/or distribute this software 
+ *   for any purpose with or without fee is hereby granted, provided 
+ *   that the above copyright notice and this permission notice appear 
+ *   in all copies.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL 
+ *   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL  
+ *   THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR 
+ *   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+ *   LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+ *   NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+ *   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *   
+ *--------------------------------------------------------------------*/
+
+/*====================================================================*
+ *   
+ *   signed VersionInfo (struct plc * plc);
+ *   
+ *   plc.h
+ * 
+ *   read hardware and software version information from an INT6x00
+ *   using a VS_SW_VER Request Message;
+ *
+ *.  Qualcomm Atheros HomePlug AV Powerline Toolkit
+ *:  Published 2009-2011 by Qualcomm Atheros. ALL RIGHTS RESERVED
+ *;  For demonstration and evaluation only. Not for production use
+ *
+ *   Contributor(s):
+ *      Charles Maier <cmaier@qualcomm.com>
+ *
+ *--------------------------------------------------------------------*/
+
+#ifndef LINKSTATUS_SOURCE
+#define LINKSTATUS_SOURCE
+
+#include <stdint.h>
+#include <memory.h>
+
+#include "../tools/error.h"
+#include "../tools/memory.h"
+#include "../tools/symbol.h"
+#include "../plc/plc.h"
+
+signed LinkStatus (struct plc * plc) 
+
+{
+	struct channel * channel = (struct channel *)(plc->channel);
+	struct message * message = (struct message *)(plc->message);
+
+#ifndef __GNUC__
+#pragma pack (push,1)
+#endif
+
+	struct __packed vs_link_status_request 
+	{
+		struct ethernet_std ethernet;
+		struct qualcomm_std qualcomm;
+	}
+	* request = (struct vs_link_status_request *) (message);
+	struct __packed vs_link_status_confirm 
+	{
+		struct ethernet_std ethernet;
+		struct qualcomm_std qualcomm;
+		uint8_t MSTATUS;
+		uint8_t LINKSTATUS;
+	}
+	* confirm = (struct vs_link_status_confirm *) (message);
+
+#ifndef __GNUC__
+#pragma pack (pop)
+#endif
+
+	Request (plc, "Request Version Information");
+	memset (message, 0, sizeof (* message));
+	EthernetHeader (&request->ethernet, channel->peer, channel->host, HOMEPLUG_MTYPE);
+	QualcommHeader (&request->qualcomm, 0, (VS_PL_LINK_STATUS | MMTYPE_REQ));
+	plc->packetsize = (ETHER_MIN_LEN - ETHER_CRC_LEN);
+	if (SendMME (plc) <= 0) 
+	{
+		error ((plc->flags & PLC_BAILOUT), errno, CHANNEL_CANTSEND);
+		return (-1);
+	}
+	while (ReadMME (plc, 0, (VS_PL_LINK_STATUS | MMTYPE_CNF)) > 0) 
+	{
+		if (confirm->MSTATUS) 
+		{
+			Failure (plc, PLC_WONTDOIT);
+			continue;
+		}
+		Display (plc, "%d", confirm->LINKSTATUS);
+	}
+	return (0);
+}
+
+
+#endif
+
