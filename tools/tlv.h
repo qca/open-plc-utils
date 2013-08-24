fe58e51e5e3244146d634083c416788c8ffbd3c5
Charles Maier on Appslinux01 <cmaier@qca.qualcomm.com>
2013-08-24 14:09:07 -0400

    Move TLVNode strcuture definition from nvm/nvm.h to tools/tlv.h in preparation for expanded use of TLVs in future MMEs.

---
+++ tools/tlv.h
@@ -0,0 +1,60 @@
+/*====================================================================*
+ *   
+ *   Copyright (c) 2011 Qualcomm Atheros Inc.
+ *   
+ *   Permission to use, copy, modify, and/or distribute this software 
+ *   for any purpose with or without fee is hereby granted, provided 
+ *   that the above copyright notice and this permission notice appear 
+ *   in all copies.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL 
+ *   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL  
+ *   THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR 
+ *   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+ *   LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+ *   NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+ *   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *   
+ *--------------------------------------------------------------------*/
+
+/*====================================================================*
+ *
+ *   tlv.h - nvm file format definitions and declarations;
+ *
+ *   Contributor(s):
+ *      Charles Maier <cmaier@qca.qualcomm.com>
+ *
+ *--------------------------------------------------------------------*/
+
+#ifndef TLV_HEADER
+#define TLV_HEADER
+
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+#ifndef __GNUC__
+#pragma pack (push,1)
+#endif
+
+typedef struct __packed TLVNode 
+
+{
+	uint32_t type;
+	uint32_t size;
+	uint32_t data;
+}
+
+TLVNode;
+
+#ifndef __GNUC__
+#pragma pack (pop)
+#endif
+
+/*====================================================================*
+ *   
+ *--------------------------------------------------------------------*/
+
+#endif
+
