e9f15adae4f2c1294774023b026125e1fb867cf4
Charles Maier on Appslinux01 <cmaier@qca.qualcomm.com>
2013-09-11 10:34:53 -0400

    Updated ReadFMI with simplified code. The function now returns one long Ethernet frame instead of a header-less payload.

--- plc/ReadFMI.c
+++ plc/ReadFMI.c
@@ -1,6 +1,6 @@
 /*====================================================================*
  *   
- *   Copyright (c) 2011 Qualcomm Atheros Inc.
+ *   Copyright (c) 2013 Qualcomm Atheros Inc.
  *   
  *   Permission to use, copy, modify, and/or distribute this software 
  *   for any purpose with or without fee is hereby granted, provided 
@@ -25,13 +25,10 @@
  *   plc.h
  *
  *   read a fragmented message and return a pointer to a buffer that
- *   contains the concatenated message fragments; The buffer address
+ *   contains the concatenated message fragments; the buffer address
  *   is returned in plc->content; the calling function must free the
- *   buffer when done;
- *
- *   buffer length is computed from the number of fragments returned
- *   in the FMI field of the first fragment; 
- *
+ *   buffer when done; buffer length is computed from the number of 
+ *   fragments returned in the FMI field of the first fragment; 
  *
  *   Contributor(s):
  *      Charles Maier <cmaier@qca.qualcomm.com>
@@ -53,71 +50,56 @@
 
 signed ReadFMI (struct plc * plc, uint8_t MMV, uint16_t MMTYPE) 
 
-{
-	struct message * message = (struct message *)(plc->message);
+{ 
+	struct message * message = (struct message *) (plc->message); 
 
 #ifndef __GNUC__
 #pragma pack (push,1)
 #endif
 
 	struct __packed frame 
-	{
-		struct ethernet_std ethernet;
-		struct homeplug_fmi homeplug;
-		uint8_t content [ETHERMTU - sizeof (struct homeplug_fmi)];
-	}
-	* frame = (struct frame *) (message);
+	{ 
+		struct ethernet_std ethernet; 
+		struct homeplug_fmi homeplug; 
+		uint8_t content [ETHERMTU - sizeof (struct homeplug_fmi)]; 
+	} 
+	* frame = (struct frame *) (message); 
 
 #ifndef __GNUC__
 #pragma pack (pop)
 #endif
 
-	byte * content = (byte *)(0);
-	unsigned count;
-	unsigned index;
-	if (ReadMME (plc, MMV, MMTYPE) <= 0) 
-	{
-		return (-1);
-	}
-	index = ((frame->homeplug.FMID >> 0) & 0x0F);
-	count = ((frame->homeplug.FMID >> 4) & 0x0F);
-
-#if 0
-
-	printf ("index=%d count=%d batch=%d\n", index, count, frame->homeplug.FMSN);
-
-#endif
-
-	content = plc->content = calloc ((count + 1), sizeof (frame->content));
-	if (!content) 
-	{
-		return (-1);
-	}
-	memcpy (content, frame->content, sizeof (frame->content));
-	content += sizeof (frame->content);
-	while (index < count) 
-	{
-		if (ReadMME (plc, MMV, MMTYPE) <= 0) 
-		{
-			free (plc->content);
-			plc->content = NULL;
-			return (-1);
-		}
-		index = ((frame->homeplug.FMID >> 0) & 0x0F);
-		count = ((frame->homeplug.FMID >> 4) & 0x0F);
-		memcpy (content, frame->content, sizeof (frame->content));
-		content += sizeof (frame->content);
-	}
-
-#if 0
-
-	hexdump (plc->content, 0, content - (byte *)(plc->content), stdout);
+	if (ReadMME (plc, MMV, MMTYPE) > 0) 
+	{ 
+		unsigned count = ((frame->homeplug.FMID >> 0) & 0x0F); 
+		unsigned extra = ((frame->homeplug.FMID >> 4) & 0x0F); 
+		unsigned length = sizeof (* frame) + extra * sizeof (frame->content); 
+		if ((plc->content = malloc (length))) 
+		{ 
+			signed offset = plc->packetsize; 
+			memcpy (plc->content, frame, offset); 
+			while (count < extra) 
+			{ 
+				if (ReadMME (plc, MMV, MMTYPE) <= 0) 
+				{ 
+					free (plc->content); 
+					plc->content = NULL; 
+					return (- 1); 
+				} 
+				count = ((frame->homeplug.FMID >> 0) & 0x0F); 
+				extra = ((frame->homeplug.FMID >> 4) & 0x0F); 
+				plc->packetsize -= sizeof (struct ethernet_std);
+				plc->packetsize -= sizeof (struct homeplug_fmi);
+				memcpy ((byte *)(plc->content) +  offset, frame->content, plc->packetsize); 
+				offset += plc->packetsize; 
+			} 
+			plc->packetsize = offset; 
+		} 
+	} 
+	return (plc->packetsize); 
+} 
 
 #endif
 
-	return ((signed)(content - (byte *)(plc->content)));
-}
 
-
-#endif
 
