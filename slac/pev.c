c2126ed98e33c2abe1e53e830e3705ed553e4eb4
Stefan Wahren <stefan.wahren@i2se.com>
2013-12-11 22:16:20 +0100

    Disable debug messages in quiet mode (fixes #28)
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- slac/pev.c
+++ slac/pev.c
@@ -476,7 +476,8 @@ int main (int argc, char const * argv [])
 			section = optarg; 
 			break; 
 		case 'q': 
-			_clrbits (channel.flags, CHANNEL_SILENCE); 
+			_setbits (channel.flags, CHANNEL_SILENCE);
+			_setbits (session.flags, SLAC_SILENCE);
 			break; 
 		case 't': 
 			channel.timeout = (unsigned) (uintspec (optarg, 0, UINT_MAX)); 
