e7b84bd29cc21a6bcf25909cb9ed7c79540a8fc8
Charles Maier <razzlefratz@gmail.com>
2015-02-04 11:50:17 -0800

    Added function NEWPassword and NEWPasswords used to produce totally random device address/password pairs for production purposes;

---
+++ key/NEWPasswords.c
@@ -0,0 +1,198 @@
+/*====================================================================*
+ *
+ *   Copyright (c) 2013 Qualcomm Atheros, Inc.
+ *
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or 
+ *   without modification, are permitted (subject to the limitations 
+ *   in the disclaimer below) provided that the following conditions 
+ *   are met:
+ *
+ *   * Redistributions of source code must retain the above copyright 
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above 
+ *     copyright notice, this list of conditions and the following 
+ *     disclaimer in the documentation and/or other materials 
+ *     provided with the distribution.
+ *
+ *   * Neither the name of Qualcomm Atheros nor the names of 
+ *     its contributors may be used to endorse or promote products 
+ *     derived from this software without specific prior written 
+ *     permission.
+ *
+ *   NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE 
+ *   GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE 
+ *   COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+ *   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ *   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER 
+ *   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ *   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ *   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ *   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ *   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ *   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+ *
+ *--------------------------------------------------------------------*/
+
+#ifndef NEWPASSWORDS_SOURCE
+#define NEWPASSWORDS_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <error.h>
+#include <errno.h>
+
+#include "../tools/types.h"
+#include "../tools/files.h"
+#include "../tools/flags.h"
+#include "../key/keys.h"
+
+/*====================================================================*
+ *
+ *   void NEWPassword (char const charset [], unsigned limit, unsigned alpha, unsigned bunch, char space);
+ *
+ *   keys.h
+ *
+ *   print a new password on stdout;
+ *
+ *   charset is an array containing an alphabet of printable ASCII
+ *   characters; limit is the charaset size in bytes; alpha is the
+ *   total number of characters to select from the alphabet; bunch
+ *   the grouping factor; space is the character used to separate
+ *   indidual groups of characters;
+ *
+ *   
+ *   
+ *
+ *--------------------------------------------------------------------*/
+
+void NEWPassword (char const charset [], unsigned limit, unsigned alpha, unsigned bunch, char space)
+
+{
+	signed fd;
+	if ((fd = open("/dev/urandom", O_BINARY | O_RDONLY)) == -1)
+	{
+		error (1, errno, "can't open /dev/urandom");
+	}
+	while (alpha--)
+	{
+		unsigned index = 0;
+		unsigned place = 5;
+		while (place--)
+		{
+			unsigned number;
+			if (read (fd, &number, sizeof (number)) != sizeof (number))
+			{
+				error (1, errno, "can't read /dev/urandom");
+			}	
+			index |= (number & 1) << place;
+		}
+		putc ( charset [index % limit], stdout);
+		if ((alpha) && (bunch) && !(alpha % bunch))
+		{
+			putc (space, stdout);
+		}
+	}
+	close (fd);
+	return;
+}
+
+/*====================================================================*
+ *
+ *   void  NEWPasswords (uint32_t vendor, uint32_t device, unsigned count, unsigned alpha, unsigned bunch, unsigned space, flag_t flags);
+ *
+ *   keys.h
+ *
+ *   print a range of device address/password pairs on stdout; print
+ *   an optional usage flag in the first column for PTS compatability;
+ *
+ *   vendor is the 24-bit OUI expressed as an integer; device is the
+ *   24-bit starting unit address expressed as an integer; count is
+ *   the number of address/password pairs to generate; alpha is the
+ *   number of letters/numbers in the password excluding delimiters;
+ *   bunch is the number of letters or numbers appear between space
+ *   characters; space characters are suppressed when bunch is zero
+ *   or greater than alpha;
+ *
+ *--------------------------------------------------------------------*/
+
+void NEWPasswords (uint32_t vendor, uint32_t device, unsigned count, unsigned alpha, unsigned bunch, char space, flag_t flags)
+
+{
+	static const char charset [] =
+	{
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'A',
+		'B',
+		'C',
+		'D',
+		'E',
+		'F',
+		'G',
+		'H',
+		'J',
+		'K',
+		'L',
+		'M',
+		'N',
+		'P',
+		'Q',
+		'R',
+		'S',
+		'T',
+		'U',
+		'V',
+		'W',
+		'X',
+		'Y',
+		'Z'
+	};
+	if (vendor >> 24)
+	{
+		return;
+	}
+	if (device >> 24)
+	{
+		return;
+	}
+	if (count >> 24)
+	{
+		return;
+	}
+	while (count--)
+	{
+		if (_anyset (flags, PASSWORD_VERBOSE))
+		{
+			putc ('0', stdout);
+			putc (' ', stdout);
+		}
+		if (_allclr (flags, PASSWORD_SILENCE))
+		{
+			printf ("%06X", vendor & 0x00FFFFFF);
+			printf ("%06X", device & 0x00FFFFFF);
+			putc (' ', stdout);
+		}
+		NEWPassword (charset, sizeof (charset), alpha, bunch, space);
+		putc ('\n', stdout);
+		device++;
+	}
+	return;
+}
+
+#endif
+
+
+
