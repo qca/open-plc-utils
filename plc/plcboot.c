qca/open-plc-utils

commit 624ed6d98aad0776b1a715d49ea0ac6a593350d0

Fixed bug introduced when we added an automatic device reset after flash memory write.

plc/plcboot.c
@@ -104,6 +104,7 @@
 #include "../plc/NVMSelect.c"
 #include "../plc/ReadMME.c"
 #include "../plc/Request.c"
+#include "../plc/ResetDevice.c"
 #include "../plc/SendMME.c"
 #include "../plc/WaitForReset.c"
 #include "../plc/WaitForStart.c"

plc/FlashSoftloader.c
@@ -74,12 +74,12 @@ signed FlashSoftloader (struct plc * plc, uint32_t options)
 		0,
 		0
 	};
-	ModuleSpec (&plc->CFG, &vs_module_spec);
+	ModuleSpec (&plc->SFT, &vs_module_spec);
 	if (ModuleSession (plc, 1, &vs_module_spec))
 	{
 		return (-1);
 	}
-	if (ModuleWrite (plc, &plc->CFG, 0, &vs_module_spec))
+	if (ModuleWrite (plc, &plc->SFT, 0, &vs_module_spec))
 	{
 		return (-1);
 	}

plc/Makefile
@@ -104,7 +104,7 @@ int6kwait: int6kwait.o Confirm.o Devices.o Display.o EthernetHeader.o Failure.o 
 mdustats: mdustats.o Devices.o Display.o EthernetHeader.o MDUTrafficStats.o MMECode.o QualcommHeader.o ReadMME.o Request.o SendMME.o UnwantedMessage.o channel.o chipset.o closechannel.o error.o getoptv.o hexdecode.o hexdump.o hexencode.o hexstring.o openchannel.o putoptv.o readpacket.o sendpacket.o set32bitmap.o synonym.o todigit.o typename.o uintspec.o version.o
 piboffset: piboffset.o
 plc: plc.o
-plcboot: plcboot.o BootDevice2.o BootFirmware2.o BootParameters2.o Confirm.o Devices.o Display.o EthernetHeader.o Failure.o FlashDevice2.o FlashFirmware.o FlashNVM.o FlashParameters.o FlashSoftloader.o InitDevice2.o MMECode.o ModuleCommit.o ModuleSession.o ModuleSpec.o ModuleWrite.o NVMSelect.o QualcommHeader.o ReadMME.o Request.o SendMME.o UnwantedMessage.o WaitForReset.o WaitForStart.o WriteExecuteApplet2.o WriteExecuteFirmware2.o WriteExecuteParameters2.o WriteFirmware2.o WriteMEM.o WriteNVM.o WritePIB.o WriteParameters2.o basespec.o channel.o checkfilename.o checksum32.o chipset.o closechannel.o error.o fdchecksum32.o getoptv.o hexdecode.o hexdump.o hexstring.o keys.o nvmfile2.o openchannel.o pibfile2.o putoptv.o readpacket.o sendpacket.o strfbits.o todigit.o typename.o uintspec.o version.o
+plcboot: plcboot.o BootDevice2.o BootFirmware2.o BootParameters2.o Confirm.o Devices.o Display.o EthernetHeader.o Failure.o FlashDevice2.o FlashFirmware.o FlashNVM.o FlashParameters.o FlashSoftloader.o InitDevice2.o MMECode.o ModuleCommit.o ModuleSession.o ModuleSpec.o ModuleWrite.o NVMSelect.o QualcommHeader.o ReadMME.o Request.o SendMME.o UnwantedMessage.o WaitForReset.o WaitForStart.o WriteExecuteApplet2.o WriteExecuteFirmware2.o WriteExecuteParameters2.o WriteFirmware2.o WriteMEM.o WriteNVM.o WritePIB.o WriteParameters2.o basespec.o channel.o checkfilename.o checksum32.o chipset.o closechannel.o error.o fdchecksum32.o getoptv.o hexdecode.o hexdump.o hexstring.o keys.o nvmfile2.o openchannel.o pibfile2.o putoptv.o readpacket.o sendpacket.o strfbits.o todigit.o typename.o uintspec.o version.o ResetDevice.o
 plcdevs: plcdevs.o Devices.o EthernetHeader.o Platform.o QualcommHeader.o UnwantedMessage.o channel.o chipset.o closechannel.o decdecode.o decstring.o error.o getoptv.o hexdecode.o hexdump.o hexstring.o hostnics.o openchannel.o putoptv.o readpacket.o sendpacket.o todigit.o typename.o uintspec.o version.o
 plcfwd: plcfwd.o Confirm.o Devices.o Display.o EthernetHeader.o Failure.o MMECode.o QualcommHeader.o ReadMME.o Request.o SendMME.o UnwantedMessage.o basespec.o binout.o channel.o closechannel.o error.o getoptv.o hexdecode.o hexdump.o hexencode.o hexout.o hexview.o openchannel.o putoptv.o readpacket.o sendpacket.o synonym.o todigit.o uintspec.o version.o
 plcget: plcget.o Confirm.o Devices.o Display.o EthernetHeader.o Failure.o GetProperty.o MMECode.o QualcommHeader.o ReadMME.o Request.o SendMME.o UnwantedMessage.o basespec.o binout.o channel.o chrout.o closechannel.o decout.o error.o getoptv.o hexdecode.o hexdump.o hexencode.o hexout.o openchannel.o putoptv.o readpacket.o sendpacket.o synonym.o todigit.o uintspec.o version.o

plc/plc.c
@@ -66,7 +66,7 @@
 struct plc plc =
 
 {
-	(struct channel *) (&channel),
+	(struct channel *) (& channel),
 	(struct message *) (0),
 	(void *) (0),
 	0,
@@ -153,7 +153,7 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
 	},
 
 /*
@@ -163,7 +163,27 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
+	},
+
+/*
+ * struct _file_ SFT; softloader file to be written to flash
+ * memory;
+ */
+
+	{
+		(file_t) (-1),
+		(char const *) (0)
+	},
+
+/*
+ * struct _file_ sft; softloader file to be read from flash
+ * memory;
+ */
+
+	{
+		(file_t) (-1),
+		(char const *) (0)
 	},
 
 /*
@@ -173,7 +193,7 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
 	},
 
 /*
@@ -186,7 +206,7 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
 	},
 
 /*
@@ -196,7 +216,7 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
 	},
 
 /*
@@ -209,7 +229,7 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
 	},
 
 /*
@@ -219,7 +239,7 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
 	},
 
 /*
@@ -229,7 +249,7 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
 	},
 
 /*
@@ -238,7 +258,7 @@ struct plc plc =
 
 	{
 		(file_t) (-1),
-		(char const *)(0)
+		(char const *) (0)
 	},
 
 /*
@@ -273,6 +293,7 @@ struct plc plc =
 	PLC_FLAGS
 };
 
-
 #endif
+
+
 

plc/plc.h
@@ -283,6 +283,8 @@ typedef struct plc
 	uint8_t DAK [HPAVKEY_DAK_LEN];
 	struct _file_ CFG;
 	struct _file_ cfg;
+	struct _file_ SFT;
+	struct _file_ sft;
 	struct _file_ NVM;
 	struct _file_ nvm;
 	struct _file_ PIB;
@@ -628,7 +630,7 @@ signed ModuleCommit (struct plc *, uint32_t flags);
 
 #ifdef __GNUC__
 
-__attribute__ ((format (printf, 2, 3)))
+__attribute__ ((format (printf, 2, 3))) 
 
 #endif
 
@@ -636,7 +638,7 @@ void Request (struct plc *, char const * format, ...);
 
 #ifdef __GNUC__
 
-__attribute__ ((format (printf, 2, 3)))
+__attribute__ ((format (printf, 2, 3))) 
 
 #endif
 
@@ -644,7 +646,7 @@ void Confirm (struct plc *, char const * format, ...);
 
 #ifdef __GNUC__
 
-__attribute__ ((format (printf, 2, 3)))
+__attribute__ ((format (printf, 2, 3))) 
 
 #endif
 
@@ -652,7 +654,7 @@ void Display (struct plc *, char const * format, ...);
 
 #ifdef __GNUC__
 
-__attribute__ ((format (printf, 2, 3)))
+__attribute__ ((format (printf, 2, 3))) 
 
 #endif
 
@@ -663,4 +665,6 @@ void Failure (struct plc *, char const * format, ...);
  *--------------------------------------------------------------------*/
 
 #endif
+
+
 

plc/plcID.c
@@ -105,7 +105,6 @@
 #include "../plc/Failure.c"
 #include "../plc/ReadMME.c"
 #include "../plc/SendMME.c"
-#include "../plc/PLCSelect.c"
 #include "../plc/WaitForStart.c"
 #include "../plc/chipset.c"
 #endif

commit bbc9bff6a84bb216fe7b092ea54daf50a7b17849

plc: Use appropriate member for Softloader data
Since introduction of plc.SFT this member should be used for
Softloader data. So fix the affected programs.

Fixes: 624ed6d98aad0776b1a715d49ea0ac6a593350d0

plc/plcboot.c
@@ -288,13 +288,13 @@ int main (int argc, char const * argv [])
 			{
 				error (1, EINVAL, "%s", optarg);
 			}
-			if ((plc.CFG.file = open (plc.CFG.name = optarg, O_BINARY|O_RDONLY)) == -1)
+			if ((plc.SFT.file = open (plc.SFT.name = optarg, O_BINARY|O_RDONLY)) == -1)
 			{
-				error (1, errno, "%s", plc.CFG.name);
+				error (1, errno, "%s", plc.SFT.name);
 			}
-			if (nvmfile2 (&plc.CFG))
+			if (nvmfile2 (&plc.SFT))
 			{
-				error (1, errno, "Bad softloader file: %s", plc.CFG.name);
+				error (1, errno, "Bad softloader file: %s", plc.SFT.name);
 			}
 			break;
 		case 't':
@@ -345,7 +345,7 @@ int main (int argc, char const * argv [])
 	{
 		error (1, ECANCELED, "No Firmware file named");
 	}
-	if (plc.CFG.file == -1)
+	if (plc.SFT.file == -1)
 	{
 		if (_anyset (plc.flags, PLC_FLASH_DEVICE))
 		{

plc/plchost.c
@@ -508,13 +508,13 @@ int main (int argc, char const * argv [])
 			{
 				error (1, EINVAL, "%s", optarg);
 			}
-			if ((plc.CFG.file = open (plc.CFG.name = optarg, O_BINARY|O_RDONLY)) == -1)
+			if ((plc.SFT.file = open (plc.SFT.name = optarg, O_BINARY|O_RDONLY)) == -1)
 			{
-				error (1, errno, "%s", plc.CFG.name);
+				error (1, errno, "%s", plc.SFT.name);
 			}
-			if (nvmfile2 (&plc.CFG))
+			if (nvmfile2 (&plc.SFT))
 			{
-				error (1, errno, "Bad firmware file: %s", plc.CFG.name);
+				error (1, errno, "Bad firmware file: %s", plc.SFT.name);
 			}
 			break;
 		case 't':
@@ -548,7 +548,7 @@ int main (int argc, char const * argv [])
 	{
 		error (1, ECANCELED, "No host PIB file named");
 	}
-	if (plc.CFG.file == -1)
+	if (plc.SFT.file == -1)
 	{
 		if (_anyset (plc.flags, PLC_FLASH_DEVICE))
 		{

plc/plchostd.c
@@ -625,13 +625,13 @@ int main (int argc, char const * argv [])
 			{
 				error (1, EINVAL, "%s", optarg);
 			}
-			if ((plc.CFG.file = open (plc.CFG.name = optarg, O_BINARY|O_RDONLY)) == -1)
+			if ((plc.SFT.file = open (plc.SFT.name = optarg, O_BINARY|O_RDONLY)) == -1)
 			{
-				error (1, errno, "%s", plc.CFG.name);
+				error (1, errno, "%s", plc.SFT.name);
 			}
-			if (nvmfile (&plc.CFG))
+			if (nvmfile (&plc.SFT))
 			{
-				error (1, errno, "Bad firmware file: %s", plc.CFG.name);
+				error (1, errno, "Bad firmware file: %s", plc.SFT.name);
 			}
 			_setbits (plc.flags, PLC_FLASH_DEVICE);
 			break;
@@ -674,7 +674,7 @@ int main (int argc, char const * argv [])
 	{
 		error (1, ECANCELED, "No user PIB file named");
 	}
-	if (plc.CFG.file == -1)
+	if (plc.SFT.file == -1)
 	{
 		if (_anyset (plc.flags, PLC_FLASH_DEVICE))
 		{

plc/plchostd2.c
@@ -624,13 +624,13 @@ int main (int argc, char const * argv [])
 			{
 				error (1, EINVAL, "%s", optarg);
 			}
-			if ((plc.CFG.file = open (plc.CFG.name = optarg, O_BINARY|O_RDONLY)) == -1)
+			if ((plc.SFT.file = open (plc.SFT.name = optarg, O_BINARY|O_RDONLY)) == -1)
 			{
-				error (1, errno, "%s", plc.CFG.name);
+				error (1, errno, "%s", plc.SFT.name);
 			}
-			if (nvmfile2 (&plc.CFG))
+			if (nvmfile2 (&plc.SFT))
 			{
-				error (1, errno, "Bad firmware file: %s", plc.CFG.name);
+				error (1, errno, "Bad firmware file: %s", plc.SFT.name);
 			}
 			break;
 		case 't':
@@ -664,7 +664,7 @@ int main (int argc, char const * argv [])
 	{
 		error (1, ECANCELED, "No host PIB file named");
 	}
-	if (plc.CFG.file == -1)
+	if (plc.SFT.file == -1)
 	{
 		if (_anyset (plc.flags, PLC_FLASH_DEVICE))
 		{

plc/plctool.c
@@ -593,13 +593,13 @@ int main (int argc, char const * argv [])
 			{
 				error (1, EINVAL, "%s", optarg);
 			}
-			if ((plc.CFG.file = open (plc.CFG.name = optarg, O_BINARY|O_RDONLY)) == -1)
+			if ((plc.SFT.file = open (plc.SFT.name = optarg, O_BINARY|O_RDONLY)) == -1)
 			{
-				error (1, errno, "%s", plc.CFG.name);
+				error (1, errno, "%s", plc.SFT.name);
 			}
-			if (nvmfile2 (&plc.CFG))
+			if (nvmfile2 (&plc.SFT))
 			{
-				error (1, errno, "Bad softloader file: %s", plc.CFG.name);
+				error (1, errno, "Bad softloader file: %s", plc.SFT.name);
 			}
 			_setbits (plc.flags, PLC_FLASH_DEVICE);
 			break;